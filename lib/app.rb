8fa9e8e69b4acdb6496edca169b6ff46c02d75da
Kane Baccigalupi <baccigalupi@gmail.com>
2015-08-15 22:29:09 -0700

    Refactor: collapses data write methods used in mutators
            The data mutators that used read methods are inlined
            to allow the readers to be updated without also updating the
            mutators.

--- lib/app.rb
+++ lib/app.rb
@@ -278,7 +278,7 @@ class App
   # write/change/delete methods for data
   def set_current_project(name)
     project_data.set_current_project(name)
-    @current_project = find_project_by_name(name)
+    @current_project = projects.detect{|project| project.keys.first == name}
   end
 
   def create_project(name)
@@ -288,38 +288,34 @@ class App
 
   def delete_project_by_name(name)
     project_data.delete(name)
-    projects.delete_if {|project| name_for_project(project) == name }.empty?
+    projects.delete_if {|project| project.keys.first == name }.empty?
   end
 
   def rename_project(old_name, new_name)
     project_data.rename(old_name, new_name)
-    current_project[new_name] = current_project_tasks
+    current_project[new_name] = current_project.values.first
     current_project.delete(old_name)
   end
 
   def add_task(name)
     project_data.add_task(name)
-    current_project_tasks << name
+    current_project.values.first << name
   end
 
   def delete_task(name)
     project_data.delete_task(name)
-    current_project_tasks.delete(name)
+    current_project.values.first.delete(name)
   end
 
   def rename_task(old_name, new_name)
     project_data.rename_task(old_name, new_name)
-    index = current_project_tasks.find_index(old_name)
-    current_project_tasks[index] = new_name
+    index = current_project.values.first.find_index(old_name)
+    current_project.values.first[index] = new_name
   end
 
   # read methods for data
-  def name_for_project(project_data)
-    project_data.keys.first
-  end
-
-  def tasks_for_project(project_data)
-    project_data.values.first
+  def name_for_project(project)
+    project.keys.first
   end
 
   def find_project_by_name(name)
@@ -327,7 +323,7 @@ class App
   end
 
   def current_project_tasks
-    tasks_for_project(current_project)
+    current_project.values.first
   end
 
   extend Forwardable
