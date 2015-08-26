7790415af174ec537b2b2763db2e5ca43267486d
Kane Baccigalupi <baccigalupi@gmail.com>
2015-08-26 08:15:35 -0700

    Refactor: Converts print methods to direct calls to Print object
            Last commit inserted the Print object into the flow. This
            one converts all the #print_... calls in App to instead call
            the Print object directly.
    
            The use of direct object method call instead of delegation
            lead to lower code metrics. My instincts are that keeping
            these as method calls to an object will make them easier to
            move around in the future. Might be wrong though.

--- lib/app.rb
+++ lib/app.rb
@@ -11,11 +11,10 @@ require_relative 'persistence'
 require_relative 'print'
 
 class App
-  attr_reader :input_stream, :output_stream,
+  attr_reader :input_stream,
     :state, :print
 
   def initialize(output_stream, input_stream)
-    @output_stream = output_stream
     @print = Print.new(output_stream)
     @input_stream = input_stream
     @state = State.new
@@ -24,124 +23,124 @@ class App
   def run
     state.load
 
-    print_projects_menu
+    print.projects_menu
     command = get_input
 
     while command != 'q'
       if !current_project?
         case command
         when 'a'
-          print_project_name_prompt
+          print.project_name_prompt
           name = get_input
           add_project(name)
-          print_project_created(name)
+          print.project_created(name)
         when 'ls'
-          print_listing_project_header
+          print.listing_project_header
           if projects_empty?
-            print_no_projects_message
+            print.no_projects_message
           else
             projects.each do |project|
-              print_project_list_item(project)
+              print.project_list_item(project)
             end
-            print_break
+            print.break
           end
         when 'd'
           if projects_empty?
-            print_cant_delete_project
-            print_no_projects_message
+            print.cant_delete_project
+            print.no_projects_message
           else
-            print_project_name_prompt
+            print.project_name_prompt
             project_name = get_input
             if project = delete_project(project_name)
-              print_successful_delete(project.name)
+              print.successful_delete(project.name)
             else
-              print_project_does_not_exist(project_name)
+              print.project_does_not_exist(project_name)
             end
           end
         when 'e'
           if projects_empty?
-            print_cant_edit_project
-            print_no_projects_message
+            print.cant_edit_project
+            print.no_projects_message
           else
-            print_project_name_prompt
+            print.project_name_prompt
             name = get_input
             if project = set_current_project(name)
-              print_tasks_menu(project.name)
+              print.tasks_menu(project.name)
               command = get_input
               next
             else
-              print_cant_edit_project
-              print_project_does_not_exist(name)
+              print.cant_edit_project
+              print.project_does_not_exist(name)
             end
           end
         end
-        print_projects_menu
+        print.projects_menu
       else
         case command
         when 'a'
-          print_task_name_prompt
+          print.task_name_prompt
           task_name = get_input
           add_task(task_name)
-          print_created_task(task_name)
+          print.created_task(task_name)
         when 'b'
           set_current_project(false)
-          print_break
+          print.break
         when 'c'
-          print_new_project_name_prompt
+          print.new_project_name_prompt
           old_name = current_project.name
           new_name = get_input
           rename_project(old_name, new_name)
-          print_changed_project_name(old_name, new_name)
+          print.changed_project_name(old_name, new_name)
         when 'e'
           name = get_input
           if task = find_task(name)
             old_name = task.name
-            print_editing_task(old_name)
-            print_task_prompt
+            print.editing_task(old_name)
+            print.task_prompt
             new_name = get_input
             rename_task(old_name, new_name)
-            print_changed_task_name(old_name, new_name)
+            print.changed_task_name(old_name, new_name)
           else
-            print_task_does_not_exsit(name)
+            print.task_does_not_exsit(name)
           end
         when 'd'
           project_name = current_project.name
           if current_tasks_empty?
-            print_no_tasks_created_in(project_name)
+            print.no_tasks_created_in(project_name)
           else
-            print_task_prompt
+            print.task_prompt
             task_name = get_input
             if task = delete_task(task_name)
-              print_task_deleted(task.name)
+              print.task_deleted(task.name)
             else
-              print_task_does_not_exsit(task_name)
+              print.task_does_not_exsit(task_name)
             end
           end
         when 'f'
           project_name = current_project.name
           if current_tasks_empty?
-            print_no_tasks_created_in(project_name)
+            print.no_tasks_created_in(project_name)
           else
-            print_task_name_prompt
+            print.task_name_prompt
             task_name = get_input
             if task = delete_task(task_name)
-              print_finished_task(task.name)
+              print.finished_task(task.name)
             else
-              print_task_does_not_exsit(task_name)
+              print.task_does_not_exsit(task_name)
             end
           end
         when 'ls'
           if current_tasks_empty?
-            print_no_tasks_created_in(current_project.name)
+            print.no_tasks_created_in(current_project.name)
           else
-            print_task_list_header
+            print.task_list_header
             current_tasks.each do |task|
-              print_task_item(task)
+              print.task_item(task)
             end
-            print_break
+            print.break
           end
         end
-        print_tasks_menu(current_project.name) if current_project?
+        print.tasks_menu(current_project.name) if current_project?
       end
 
       command = get_input
@@ -150,138 +149,6 @@ class App
 
   def get_input
     input_stream.gets.strip
-  end
-
-  def print_line(message='')
-    output_stream.puts(message)
-  end
-
-  # Menu Headers
-
-  def print_projects_menu
-    print_line("\e[38;5;40mWelcome to Taskitome!")
-    print_line("\e[0;37m=============================\n")
-    print_line("\e[0mPROJECTS MENU")
-    print_line("\e[0;37m-----------------------------")
-    print_line("\e[40;38;5;214mENTER A COMMAND:\e[0m")
-    print_line("\e[1;37ma   \e[0;35mAdd a new project")
-    print_line("\e[1;37mls  \e[0;35mList all project")
-    print_line("\e[1;37md   \e[0;35mDelete a project")
-    print_line("\e[1;37me   \e[0;35mEdit a project")
-    print_line("\e[1;37mq   \e[0;35mQuit the app\e[0m\n\n")
-  end
-
-  def print_tasks_menu(project_name)
-    print_line("\e[38;5;40mEditing project: '#{project_name}'\n\n")
-    print_line("\e[0;37mEDIT PROJECT MENU\e[0m")
-    print_line("-----------------------------")
-    print_line("\e[40;38;5;214mENTER A COMMAND:\e[0m")
-    print_line("\e[1;37mc   \e[0;35mChange the project name")
-    print_line("\e[1;37ma   \e[0;35mAdd a new task")
-    print_line("\e[1;37mls  \e[0;35mList all tasks")
-    print_line("\e[1;37md   \e[0;35mDelete a task")
-    print_line("\e[1;37me   \e[0;35mEdit a task")
-    print_line("\e[1;37mf   \e[0;35mFinish a task")
-    print_line("\e[1;37mb   \e[0;35mBack to Projects menu")
-    print_line("\e[1;37mq   \e[0;35mQuit the app\e[0m\n\n")
-  end
-
-  # Assorted printing/view methods
-  def print_break
-    print_line("\n\n")
-  end
-
-  # project menu related messages, no arguments required
-  def print_no_projects_message
-    print_line("\e[40;38;5;214mNo projects created\e[0m\n\n")
-  end
-
-  def print_project_name_prompt
-    print_line("\e[0;3mEnter a project name:\e[0m")
-  end
-
-  def print_new_project_name_prompt
-    print_line("\e[0;35mEnter new project name:\e[0m")
-  end
-
-  def print_listing_project_header
-    print_line("\e[38;5;40mListing projects:\e[0m\n")
-  end
-
-  def print_cant_delete_project
-    print_line("\e[40;38;5;214mCan't delete a project\e[0m")
-  end
-
-  def print_cant_edit_project
-    print_line("\e[40;38;5;214mCan't edit any projects\e[0m")
-  end
-
-  # project menu related messages, with data passed in
-  def print_project_created(name)
-    print_line("\e[38;5;40mCreated project:\e[0m '#{name}'\n\n")
-  end
-
-  def print_project_list_item(project)
-    print_line("  #{project.position}.  \e[0;35m#{project.name}\e[0m\n")
-  end
-
-  def print_successful_delete(name)
-    print_line "\e[38;5;40mDeleting project:\e[0m '#{name}'\n\n"
-  end
-
-  def print_project_does_not_exist(name)
-    print_line "\e[40;38;5;214mProject doesn't exist:\e[0m '#{name}'\n\n"
-  end
-
-  # task menu related messages, no data passed in
-  def print_task_name_prompt
-    print_line("\e[0;35mEnter a task name:\e[0m")
-  end
-
-  def print_task_list_header
-    print_line("\e[38;5;40mListing tasks:\e[0m")
-  end
-
-  # task menu related messages, data required
-
-  def print_created_task(name)
-    print_line("\e[38;5;40mCreated task:\e[0m '#{name}'\n\n")
-  end
-
-  def print_changed_project_name(old_name, new_name)
-    print_line("\e[38;5;40mChanged project name from\e[0m '#{old_name}' \e[38;5;40mto\e[0m '#{new_name}'\n\n")
-  end
-
-  def print_editing_task(name)
-    print_line("\e[38;5;40mEditing task:\e[0m '#{name}'")
-  end
-
-  def print_task_prompt
-    print_line("\e[0;35mEnter a task name:\e[0m")
-  end
-
-  def print_changed_task_name(old_name, new_name)
-    print_line("\e[38;5;40mChanged task name from\e[0m '#{old_name}' \e[38;5;40mto\e[0m '#{new_name}'\n\n")
-  end
-
-  def print_task_does_not_exsit(name)
-    print_line("\e[40;38;5;214mTask doesn't exist:\e[0m '#{name}'\n\n")
-  end
-
-  def print_no_tasks_created_in(project_name)
-    print_line("\e[40;38;5;214mNo tasks created in '#{project_name}'\e[0m\n\n")
-  end
-
-  def print_task_deleted(name)
-    print_line("\e[38;5;40mDeleted task:\e[0m '#{name}'\n\n")
-  end
-
-  def print_finished_task(name)
-    print_line("\e[38;5;40mFinished task:\e[0m '#{name}'\n\n")
-  end
-
-  def print_task_item(task)
-    print_line("  #{task.position}.  \e[0;35m#{task.name}\e[0m")
   end
 
   extend Forwardable
--- lib/print.rb
+++ lib/print.rb
@@ -1,3 +1,132 @@
 class Print < Struct.new(:output_stream)
-  
+  def projects_menu
+    print_line("\e[38;5;40mWelcome to Taskitome!")
+    print_line("\e[0;37m=============================\n")
+    print_line("\e[0mPROJECTS MENU")
+    print_line("\e[0;37m-----------------------------")
+    print_line("\e[40;38;5;214mENTER A COMMAND:\e[0m")
+    print_line("\e[1;37ma   \e[0;35mAdd a new project")
+    print_line("\e[1;37mls  \e[0;35mList all project")
+    print_line("\e[1;37md   \e[0;35mDelete a project")
+    print_line("\e[1;37me   \e[0;35mEdit a project")
+    print_line("\e[1;37mq   \e[0;35mQuit the app\e[0m\n\n")
+  end
+
+  def tasks_menu(project_name)
+    print_line("\e[38;5;40mEditing project: '#{project_name}'\n\n")
+    print_line("\e[0;37mEDIT PROJECT MENU\e[0m")
+    print_line("-----------------------------")
+    print_line("\e[40;38;5;214mENTER A COMMAND:\e[0m")
+    print_line("\e[1;37mc   \e[0;35mChange the project name")
+    print_line("\e[1;37ma   \e[0;35mAdd a new task")
+    print_line("\e[1;37mls  \e[0;35mList all tasks")
+    print_line("\e[1;37md   \e[0;35mDelete a task")
+    print_line("\e[1;37me   \e[0;35mEdit a task")
+    print_line("\e[1;37mf   \e[0;35mFinish a task")
+    print_line("\e[1;37mb   \e[0;35mBack to Projects menu")
+    print_line("\e[1;37mq   \e[0;35mQuit the app\e[0m\n\n")
+  end
+
+  # Assorted printing/view methods
+  def break
+    print_line("\n\n")
+  end
+
+  # project menu related messages, no arguments required
+  def no_projects_message
+    print_line("\e[40;38;5;214mNo projects created\e[0m\n\n")
+  end
+
+  def project_name_prompt
+    print_line("\e[0;3mEnter a project name:\e[0m")
+  end
+
+  def new_project_name_prompt
+    print_line("\e[0;35mEnter new project name:\e[0m")
+  end
+
+  def listing_project_header
+    print_line("\e[38;5;40mListing projects:\e[0m\n")
+  end
+
+  def cant_delete_project
+    print_line("\e[40;38;5;214mCan't delete a project\e[0m")
+  end
+
+  def cant_edit_project
+    print_line("\e[40;38;5;214mCan't edit any projects\e[0m")
+  end
+
+  # project menu related messages, with data passed in
+  def project_created(name)
+    print_line("\e[38;5;40mCreated project:\e[0m '#{name}'\n\n")
+  end
+
+  def project_list_item(project)
+    print_line("  #{project.position}.  \e[0;35m#{project.name}\e[0m\n")
+  end
+
+  def successful_delete(name)
+    print_line "\e[38;5;40mDeleting project:\e[0m '#{name}'\n\n"
+  end
+
+  def project_does_not_exist(name)
+    print_line "\e[40;38;5;214mProject doesn't exist:\e[0m '#{name}'\n\n"
+  end
+
+  # task menu related messages, no data passed in
+  def task_name_prompt
+    print_line("\e[0;35mEnter a task name:\e[0m")
+  end
+
+  def task_list_header
+    print_line("\e[38;5;40mListing tasks:\e[0m")
+  end
+
+  # task menu related messages, data required
+  def created_task(name)
+    print_line("\e[38;5;40mCreated task:\e[0m '#{name}'\n\n")
+  end
+
+  def changed_project_name(old_name, new_name)
+    print_line("\e[38;5;40mChanged project name from\e[0m '#{old_name}' \e[38;5;40mto\e[0m '#{new_name}'\n\n")
+  end
+
+  def editing_task(name)
+    print_line("\e[38;5;40mEditing task:\e[0m '#{name}'")
+  end
+
+  def task_prompt
+    print_line("\e[0;35mEnter a task name:\e[0m")
+  end
+
+  def changed_task_name(old_name, new_name)
+    print_line("\e[38;5;40mChanged task name from\e[0m '#{old_name}' \e[38;5;40mto\e[0m '#{new_name}'\n\n")
+  end
+
+  def task_does_not_exsit(name)
+    print_line("\e[40;38;5;214mTask doesn't exist:\e[0m '#{name}'\n\n")
+  end
+
+  def no_tasks_created_in(project_name)
+    print_line("\e[40;38;5;214mNo tasks created in '#{project_name}'\e[0m\n\n")
+  end
+
+  def task_deleted(name)
+    print_line("\e[38;5;40mDeleted task:\e[0m '#{name}'\n\n")
+  end
+
+  def finished_task(name)
+    print_line("\e[38;5;40mFinished task:\e[0m '#{name}'\n\n")
+  end
+
+  def task_item(task)
+    print_line("  #{task.position}.  \e[0;35m#{task.name}\e[0m")
+  end
+
+  private
+
+  def print_line(message='')
+    output_stream.puts(message)
+  end
 end
